592938464c298e80dccebb0997dc3430375319c7
Piotr Jakubowski <piotrj@gmail.com>
2011-06-14 11:38:34 +0200

    Turns out that keeping classes as hashes keys is not very good idea.
    Using symbols instead. Using two separate classes in case someone uses
    the symbol that is identical to class name in his hash.

--- lib/superclass_hash.rb
+++ lib/superclass_hash.rb
@@ -1,22 +1,27 @@
 class SuperclassHash
   def initialize()
-    @hash = {}
+    @normal_hash = {}
+    @classes_hash = {}
   end
 
   def [](key)
     unless key.is_a? Class
-      return @hash[key]
+      return @normal_hash[key]
     else
       ancestors = key.ancestors
-      ancestors = ancestors[0..ancestors.index(Object)] #Going only as far as you can via superclass method
+      ancestors = ancestors[0..ancestors.index(Object)].map { |klass| klass.to_s.to_sym } #Going only as far as you can via superclass method
       ancestors.each do |klass|
-        return @hash[klass] if @hash.has_key?(klass)
+        return @classes_hash[klass] if @classes_hash.has_key?(klass)
       end
       return nil
     end
   end
 
   def []=(key, value)
-    @hash[key] = value
+    if key.is_a? Class
+      @classes_hash[key.to_s.to_sym] = value
+    else
+      @normal_hash[key] = value
+    end
   end
 end
